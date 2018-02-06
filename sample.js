ca4428b2b6258197bae18432c02f7c2d48c4bc67
John Deck <jdeck88@gmail.com>
2018-02-05 16:02:52 -0800

    providing a nice example to use with node

---
+++ sample.js
@@ -0,0 +1,13 @@
+/*
+Sample script to load a sample JSON file for use in
+developing stats 
+
+To get started
+1. npm install require
+2. node sample.js
+*/
+
+var fs = require('fs');
+var dataRaw = fs.readFileSync('./data.json');
+var data = JSON.parse(dataRaw);
+console.log(data);
