815539b2f91bf99538b7873ff0a65e9739127d74
John Deck <jdeck88@gmail.com>
2018-02-06 16:21:06 -0800

    expanding the sample.js file a bit, now utilizing the underscore library

--- sample.js
+++ sample.js
@@ -4,10 +4,26 @@ developing stats
 
 To get started
 1. npm install require
-2. node sample.js
+2. npm install underscore
+3. node sample.js
 */
 
 var fs = require('fs');
+var _ = require('underscore')._;
 var dataRaw = fs.readFileSync('./data-small.json');
 var data = JSON.parse(dataRaw);
+
+console.log("---------this is our incoming data source ------------")
 console.log(data);
+
+
+var grouper = function(row) { return row.type; };
+var summer = function(rows) { return rows.reduce(function(sum, row) { return sum + row.weight; }, 0); };
+
+var o  = _.chain(data)
+          .groupBy(grouper)
+          .map(function(rows) { return { type: rows[0].type,  sum: summer(rows) }; })
+          .value();
+
+console.log("---------output stats------------")
+console.log(o); // same result
