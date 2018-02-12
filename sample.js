8619efdad3e3fe3509b44f04218c7595c18a86e3
mariadeck <m.elenadeck15@gmail.com>
2018-02-12 13:45:51 -0800

    made a value counter function for statistics tab

--- sample.js
+++ sample.js
@@ -4,26 +4,46 @@ developing stats
 
 To get started
 1. npm install require
-2. npm install underscore
-3. node sample.js
+3. npm install d3
+4. node sample.js
 */
 
 var fs = require('fs');
-var _ = require('underscore')._;
-var dataRaw = fs.readFileSync('./data-small.json');
+var dataRaw = fs.readFileSync('./data.json');
 var data = JSON.parse(dataRaw);
+var d3 = require('d3');
 
 console.log("---------this is our incoming data source ------------")
 console.log(data);
 
+console.log("---------this is our output data -----")
 
-var grouper = function(row) { return row.type; };
-var summer = function(rows) { return rows.reduce(function(sum, row) { return sum + row.weight; }, 0); };
+console.log(JSON.stringify(valueTotal(data,'institutionCode','key','ascending','collectionCode')));
 
-var o  = _.chain(data)
-          .groupBy(grouper)
-          .map(function(rows) { return { type: rows[0].type,  sum: summer(rows) }; })
-          .value();
+// Group on a name and return the number of counts for each name in the dataset
+// parameters are:
+// 1. JSON object containing data 
+// 2. a name containing an attribute in the JSON Object
+// 3. sortTopic "key" or "value"
+// 4. sortDirection "ascending" or "descending"
+// 5. nestedName, another name to nest
+function valueTotal(data, name, sortTopic, sortDirection, nestedName) {
+	var groupData;
+	if (nestedName != null) {
+		groupData = d3.nest()
+		.key(function(d) { return eval('d.'+name); })
+		.key(function(d) { return eval('d.'+nestedName); })
+		.rollup(function(v) {return v.length; })
+		.entries(data)
+	} else {
+		groupData = d3.nest()
+		.key(function(d) { return eval('d.'+name); })
+		.rollup(function(v) {return v.length; })
+		.entries(data)
+	}
+	// sort by key,value and ascending,descending
+	return groupData.sort(function(x,y) {
+		return eval('d3.'+sortDirection+'(x.'+sortTopic+',y.'+sortTopic+')');
+	});
+}
 
-console.log("---------output stats------------")
-console.log(o); // same result
