0e5067311c10f4eeac9ed6a4779ced515316e5bb
John Deck <jdeck88@gmail.com>
2018-02-13 20:39:32 -0800

    changing group functions with nestedName to return name:nestedName instead of an object within an object

--- sample.js
+++ sample.js
@@ -13,12 +13,11 @@ var dataRaw = fs.readFileSync('./data.json');
 var data = JSON.parse(dataRaw);
 var d3 = require('d3');
 
-console.log("---------this is our incoming data source ------------")
-console.log(data);
+console.log("---------group by institution code and collection code, and sorted by Institution Code Name-----")
+console.log(JSON.stringify(valueTotal(data,'institutionCode', 'collectionCode', 'key','ascending')));
 
-console.log("---------fthe following sorts by institution code and collection code, sorted by Institution Code Name-----")
-
-console.log(JSON.stringify(valueTotal(data,'institutionCode','key','ascending','collectionCode')));
+console.log("---------group by BasisOfRecord and sorts by value-----")
+console.log(JSON.stringify(valueTotal(data,'basisOfRecord',null,'key','ascending')));
 
 // Group on a name and return the number of counts for each name in the dataset
 // parameters are:
@@ -27,12 +26,11 @@ console.log(JSON.stringify(valueTotal(data,'institutionCode','key','ascending','
 // 3. sortTopic "key" or "value"
 // 4. sortDirection "ascending" or "descending"
 // 5. nestedName, another name to nest
-function valueTotal(data, name, sortTopic, sortDirection, nestedName) {
+function valueTotal(data, name, nestedName, sortTopic, sortDirection ) {
 	var groupData;
 	if (nestedName != null) {
 		groupData = d3.nest()
-		.key(function(d) { return eval('d.'+name); })
-		.key(function(d) { return eval('d.'+nestedName); })
+		.key(function(d) { return eval('d.'+name) + ":" + eval('d.'+nestedName); })
 		.rollup(function(v) {return v.length; })
 		.entries(data)
 	} else {
